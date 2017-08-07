700c6c17fbf8471969253474c933cfe13af1bee1
cavis <ryancavis@gmail.com>
2017-08-07 11:41:18 -0600

    Better metrics

--- test/coverage-test.js
+++ test/coverage-test.js
@@ -1,30 +1,54 @@
 const support = require('./support');
 const podagent = require('../index');
 
+const printTotal = (count, total) => {
+  return (count / total * 100).toFixed(1) + `% (${count} / ${total})`;
+}
+const countIf = (countFn, totalFn) => {
+  let count = 0, countWeight = 0, total = 0, totalWeight = 0;
+  support.eachAgent((agent, weight) => {
+    let data = podagent.parse(agent);
+    if (countFn(data)) {
+      count++;
+      countWeight += weight;
+    }
+    if (!totalFn || totalFn(data)) {
+      total++;
+      totalWeight += weight;
+    }
+  });
+  alog(`Total: ${printTotal(count, total)}`);
+  alog(`Weighted: ${printTotal(countWeight, totalWeight)}`);
+}
+
 describe('coverage-test', function() {
 
   it('total coverage', () => {
-    let count = 0, weighted = 0;
-    support.eachAgent((agent, weight) => {
-      if (podagent.parse(agent)) {
-        count++;
-        weighted += weight;
-      }
-    });
-    alog(`Total: ${support.total(count)}`)
-    alog(`Weighted: ${support.weight(weighted)}`)
+    countIf(
+      agent => agent,
+      agent => !agent || (!agent.bot && !agent.blank)
+    );
   });
 
-  it('non-bot coverage', () => {
-    let count = 0, weighted = 0;
-    support.eachAgent((agent, weight) => {
-      if (!support.isBot(agent) && podagent.parse(agent)) {
-        count++;
-        weighted += weight;
-      }
-    });
-    alog(`Total: ${support.total(count, true)}`)
-    alog(`Weighted: ${support.weight(weighted, true)}`)
+  it('name coverage', () => {
+    countIf(
+      agent => agent && agent.name,
+      agent => !agent || (!agent.bot && !agent.blank)
+    );
+  });
+
+  it('type coverage', () => {
+    countIf(
+      agent => agent && agent.type,
+      agent => !agent || (!agent.bot && !agent.blank)
+    );
+  });
+
+  it('os coverage', () => {
+    countIf(
+      agent => agent && agent.os,
+      agent => !agent || (!agent.bot && !agent.blank)
+    );
   });
 
 });
--- test/support/index.js
+++ test/support/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const csv = require('csv');
-const bot = require('./bot');
 const mocha = require('mocha');
 
 // log statements AFTER the test name prints
@@ -30,40 +29,18 @@ exports.elog = msg => {
 }
 global.elog = exports.elog;
 
-// test for bots
-exports.isBot = bot.isBot;
-
 // agent map
 const agentLookup = {}
 exports.eachAgent = fn => Object.keys(agentLookup).forEach(a => fn(a, agentLookup[a]));
 
 // test agent string csv loader
-let agentTotal = 0, agentWeight = 0, nonBotTotal = 0, nonBotWeight = 0;
 before(function(done) {
   this.timeout(10000);
   process.stdout.write('  loading testagents.csv... ');
   let parser = csv.parse({columns: true}, (err, data) => {
     console.log(`got ${data.length} agents!\n`);
     data.forEach(data => agentLookup[data.user_agent] = +data.count);
-    exports.eachAgent((agent, weight) => {
-      agentTotal++;
-      agentWeight += weight;
-      if (!bot.isBot(agent)) {
-        nonBotTotal++;
-        nonBotWeight += weight;
-      }
-    });
     done(err);
   });
   fs.createReadStream(`${__dirname}/testagents.csv`).pipe(parser);
 });
-
-// helpers to print total
-exports.total = (count, noBots) => {
-  let total = noBots ? nonBotTotal : agentTotal;
-  return (count / agentTotal * 100).toFixed(1) + `% (${count} / ${total})`;
-}
-exports.weight = (count, noBots) => {
-  let weight = noBots ? nonBotWeight : agentWeight;
-  return (count / weight * 100).toFixed(1) + `% (${count} / ${weight})`;
-}
