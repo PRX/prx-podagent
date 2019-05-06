065ae4560d023ea00d86ec0e088a5ba38c3ae805
cavis <ryancavis@gmail.com>
2019-05-06 10:05:22 -0600

    Lock to simple js file

--- index.js
+++ index.js
@@ -1,66 +1,46 @@
 const fs = require('fs');
-const yaml = require('js-yaml');
-const TAGS = ['name', 'type', 'os'];
+const db = require('./db/agents.js');
 
 /**
- * Sync parse a user agent string
+ * Parse a user agent string
  */
 exports.parse = (agentStr, callback) => {
+  const match = db.matchers.find(m => m[0].test(agentStr));
+  const data = exports.format(match);
   if (callback) {
-    exports.load(err => {
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, findAgent(agentStr));
-      }
-    });
-  } else {
-    exports.load();
-    return findAgent(agentStr);
+    callback(null, data);
   }
+  return data;
 }
 
 /**
- * Load the agents database
+ * Return _all_ matches for a user agent, not just the first
  */
-let DB = false;
-exports.load = (callback) => {
-  if (DB) {
-    callback && callback();
-  } else if (callback) {
-    fs.readFile(`${__dirname}/db/agents.lock.yml`, (err, text) => {
-      parseAgents(text);
-      callback(err);
-    });
-  } else {
-    parseAgents(fs.readFileSync(`${__dirname}/db/agents.lock.yml`, 'utf8'));
+exports.parseAll = (agentStr, callback) => {
+  const matches = db.matchers.filter(m => m[0].test(agentStr));
+  const datas = matches.map.map(m => exports.format(m));
+  if (callback) {
+    callback(null, datas);
   }
+  return datas;
 }
 
 /**
- * Build regex from strings
+ * Nicely format a matcher
  */
- function parseAgents(text) {
-   let data = yaml.safeLoad(text) || {};
-   let agents = data.agents || [];
-   let tagLookup = data.tags || [];
-   DB = agents.map(agent => {
-     if (agent.caseInsensitive) {
-       agent.regex = new RegExp(agent.regex, 'i');
-     } else {
-       agent.regex = new RegExp(agent.regex);
-     }
-     TAGS.forEach(t => {
-       agent[`${t}Id`] = parseInt(agent[t]) || null;
-       agent[t] = tagLookup[agent[t]] || null;
-     });
-     return agent;
-   });
- }
-
-/**
- * Find and agent in the database
- */
-function findAgent(agentStr) {
-  return DB.find(agent => agent.regex.test(agentStr)) || null;
+exports.format = (match) => {
+  if (match) {
+    return {
+      regex: match[0],
+      name: db.tags[match[1]] || null,
+      type: db.tags[match[2]] || null,
+      os: db.tags[match[3]] || null,
+      nameId: match[1] || null,
+      typeId: match[2] || null,
+      osId: match[3] || null,
+      bot: match[4] || false,
+    };
+  } else {
+    return null;
+  }
 }
