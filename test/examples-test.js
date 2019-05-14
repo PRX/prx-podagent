dc40d364e82baef8b138681e1271ee83179c7218
cavis <ryancavis@gmail.com>
2019-05-14 16:23:34 -0600

    Test for examples

---
+++ test/examples-test.js
@@ -0,0 +1,42 @@
+const expect = require('chai').expect;
+const support = require('./support');
+const podagent = require('../index');
+
+describe('examples-test', function() {
+
+  it('all examples match their regex', () => {
+    let nonMatching = 0;
+
+    support.eachExample((str, idx) => {
+      const agent = podagent.parse(str);
+      const matcher = podagent.db.matchers[idx][0];
+
+      if (!agent) {
+        console.log(str);
+        alog(`${str} => no matches ${matcher}`);
+        nonMatching++;
+      } else if (agent.index !== idx) {
+        alog(`${str} => matches ${agent.regex} (${agent.index}) instead of ${matcher} (${idx})`);
+        nonMatching++;
+      }
+    });
+
+    expect(nonMatching).to.equal(0);
+  });
+
+  it('all regexps have an example', () => {
+    let hasExamples = {};
+    support.eachExample((str, idx) => hasExamples[idx] = true);
+
+    let missingExamples = 0;
+    podagent.db.matchers.forEach((m, idx) => {
+      if (!hasExamples[idx]) {
+        missingExamples++;
+        alog(`${m[0]} => no examples`);
+      }
+    });
+
+    expect(missingExamples).to.equal(0);
+  });
+
+});
