d08299efdc4f0381c7fca2cbff95c26aa9f7b1e9
cavis <ryancavis@gmail.com>
2019-06-28 11:38:58 -0600

    Also copy non-lock file to json

--- lock/lock-json.js
+++ lock/lock-json.js
@@ -4,13 +4,18 @@ const yaml = require('js-yaml')
 /**
  * Convert lockfile to javascript agents.lock.yml -> agents.json
  */
+const fullFile = `${__dirname}/../db/agents.yml`
 const lockFile = `${__dirname}/../db/agents.lock.yml`
-const outFile = `${__dirname}/../db/agents.lock.json`
+const outFull = `${__dirname}/../db/agents.json`
+const outLock = `${__dirname}/../db/agents.lock.json`
+const fullText = fs.readFileSync(fullFile, 'utf8')
 const lockText = fs.readFileSync(lockFile, 'utf8')
+const fullData = yaml.safeLoad(fullText) || {}
 const lockData = yaml.safeLoad(lockText) || {}
 
 // write to file
 console.log('write json database...')
-console.log(`  ${lockData.agents.length} agents`)
+console.log(`  ${fullData.agents.length} agents`)
 console.log(`  ${Object.keys(lockData.tags).length} tags`)
-fs.writeFileSync(outFile, JSON.stringify(lockData, null, 2))
+fs.writeFileSync(outFull, JSON.stringify(fullData, null, 2))
+fs.writeFileSync(outLock, JSON.stringify(lockData, null, 2))
